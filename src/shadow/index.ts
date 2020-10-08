e3eaa901f1d327754f1dc574a1ab3b8d2c0f6d2a
Rogin Farrer <rogin@roginfarrer.com>
2020-10-07 23:29:36 -0400

    added shadow

---
+++ src/shadow/index.ts
@@ -0,0 +1,33 @@
+import { betterGet } from '../get';
+import { Transform, PropConfigCollection } from '../types';
+
+const getShadow: Transform = (value, _, props) => {
+  let result = betterGet(props?.theme?.shadows, value);
+  if (result) {
+    return result;
+  }
+  if (typeof value === 'string') {
+    const arr = value.split(' ');
+    const maybeColor = arr.pop();
+    const foundColor = betterGet(props?.theme?.colors, maybeColor);
+    if (foundColor) {
+      return `${arr.join(' ')} ${foundColor}`;
+    }
+  }
+  return value;
+};
+
+export const shadow: PropConfigCollection = {
+  boxShadow: {
+    property: 'boxShadow',
+    scale: 'shadows',
+    transform: getShadow,
+  },
+  textShadow: {
+    property: 'textShadow',
+    scale: 'shadows',
+    transform: getShadow,
+  },
+};
+
+export default shadow;

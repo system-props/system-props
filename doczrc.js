5684b28d48aba6b805c09b650a1e0bad406bf8b0
Rogin Farrer <rogin@roginfarrer.com>
2020-10-28 22:00:14 -0400

    adding more docs

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,21 @@
 export default {
 	title: 'System Props',
-	dest: 'site',
 	src: 'docs',
 	typescript: true,
 	editBranch: 'main',
+	menu: ['System Props', 'Getting Started'],
+	themeConfig: {
+		space: [
+			'0px',
+			'4px',
+			'8px',
+			'16px',
+			'32px',
+			'48px',
+			'64px',
+			'80px',
+			'100px',
+		],
+		fontSizes: ['12px', '14px', '16px', '20px', '24px', '32px', '48px', '64px'],
+	},
 };

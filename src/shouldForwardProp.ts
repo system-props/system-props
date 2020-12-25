89d0a1cc9743e5b6277c58e490690960d81303a6
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-12-24 16:16:40 -0800

    Remove console.log from shouldForwardProp

--- src/shouldForwardProp.ts
+++ src/shouldForwardProp.ts
@@ -2,7 +2,5 @@ import { propNames } from './propNames';
 
 const allPropNames = new Set(propNames);
 
-console.log({ allPropNames });
-
 export const shouldForwardProp = (prop: string): boolean =>
   !allPropNames.has(prop);

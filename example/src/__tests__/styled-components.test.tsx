cbb23a2845d7f83e5f26f97e72eafbe54087c3db
Rogin Farrer <rogin@roginfarrer.com>
2021-05-31 15:21:01 -0400

    try to fix ci

--- example/src/__tests__/styled-components.test.tsx
+++ example/src/__tests__/styled-components.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Box } from '../box-styled-components';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import 'jest-styled-components';
 
 describe('pseudo selectors', () => {

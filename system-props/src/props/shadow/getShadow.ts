d2f68d645039f3f63bab7a42833a6fa7ee674555
Rogin Farrer <rogin@roginfarrer.com>
2021-10-09 23:00:01 -0400

    shadow and transition

--- system-props/src/props/shadow/getShadow.ts
+++ system-props/src/props/shadow/getShadow.ts
@@ -1,7 +1,7 @@
 import { Transform } from '../../core/types';
 import { tokenizeValue } from '../tokenizeValue';
 
-export const getShadow: Transform = ({ path, object, get, props }) => {
+export const getShadow: Transform = ({ path, object, get, props, strict }) => {
   let result = get(object, path);
   if (result) {
     return result;
@@ -9,7 +9,11 @@ export const getShadow: Transform = ({ path, object, get, props }) => {
   if (typeof path === 'string') {
     return tokenizeValue(path)
       .map((chain) =>
-        chain.map((val) => get(props?.theme?.colors, val, val)).join(' ')
+        chain
+          .map((val) =>
+            get(props?.theme?.colors, val, strict && !!object ? undefined : val)
+          )
+          .join(' ')
       )
       .join(', ');
   }
